d9db4b24c8a3c6d1261c1a712999628bb194329c
kboul <kos.voulgaridis@gmail.com>
2022-12-09 23:37:52 +0200

    Style fix for mobiles with small screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     screens: {
-      sm: { min: "500px", max: "767px" },
+      sm: { min: "350px", max: "767px" },
       // => @media (min-width: 640px and max-width: 767px) { ... }
       md: { min: "768px", max: "1023px" }
       // => @media (min-width: 768px and max-width: 1023px) { ... }
