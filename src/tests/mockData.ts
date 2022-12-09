e20ad845471615955d9aa0e16f3feaceef0d0f18
kboul <kos.voulgaridis@gmail.com>
2022-12-10 00:05:02 +0200

    Setup msw mockData for users, create renderWithQueryClient testing wrapper

--- src/tests/mockData.ts
+++ src/tests/mockData.ts
@@ -1,1 +1,11 @@
-export const users = [];
+export const users = [
+  {
+    id: "5c093af1c6ee9117a581c7d6",
+    photo: "https://randomuser.me/api/portraits/men/40.jpg",
+    name: "Bates Washington",
+    company: "ZOLAREX",
+    email: "bates.washington@zolarex.io",
+    phone: "+1 (915) 447-2207",
+    address: "958 Brevoort Place, Ona, Maine, 2433"
+  }
+];
