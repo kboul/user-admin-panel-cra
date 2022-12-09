4bd85bc4991357f1c030c11fcffc9b08f9419379
kboul <kos.voulgaridis@gmail.com>
2022-12-10 00:24:51 +0200

    Unit test 3 scenarios for user display on LeftPanel

---
+++ src/components/UserPanel/LeftPanel.test.tsx
@@ -0,0 +1,40 @@
+import { rest } from "msw";
+import { baseUrl } from "../../api/baseUrl";
+import { users } from "../../tests/mockData";
+import {
+  renderWithQueryClient,
+  screen,
+  waitFor
+} from "../../tests/renderWithQueryClient";
+import server from "../../tests/server";
+
+import LeftPanel from "./LeftPanel";
+
+test("loading message appears on the screen when components mounts", () => {
+  renderWithQueryClient(<LeftPanel />);
+  expect(screen.getByText("Loading users...")).toBeInTheDocument();
+});
+
+test("users appear on the screen", async () => {
+  renderWithQueryClient(<LeftPanel />);
+
+  const userPhotos = await screen.findAllByRole("img");
+  users.forEach(async (user, index) => {
+    expect(await screen.findByText(user.name)).toBeInTheDocument();
+    expect(await screen.findByText(user.email)).toBeInTheDocument();
+    expect(userPhotos[index].getAttribute("src")).toBe(user.photo);
+  });
+});
+
+test("server error generates the appropriate message on the screen", async () => {
+  server.resetHandlers(
+    rest.get(baseUrl, (_, res, ctx) => res(ctx.status(500)))
+  );
+  renderWithQueryClient(<LeftPanel />);
+
+  await waitFor(async () => {
+    expect(
+      await screen.findByText("There was an error while fetching the users.")
+    ).toBeInTheDocument();
+  });
+});
--- src/tests/mockData.ts
+++ src/tests/mockData.ts
@@ -7,5 +7,14 @@ export const users = [
     email: "bates.washington@zolarex.io",
     phone: "+1 (915) 447-2207",
     address: "958 Brevoort Place, Ona, Maine, 2433"
+  },
+  {
+    id: "5c093af1aeca1bb00607fb2a",
+    photo: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Mollie Oneill",
+    company: "VIAGRAND",
+    email: "mollie.oneill@viagrand.biz",
+    phone: "+1 (852) 535-3880",
+    address: "120 Cedar Street, Mansfield, Kentucky, 8890"
   }
 ];
