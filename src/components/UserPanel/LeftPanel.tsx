99c360cc5e21922f7c15cac2cf88ac5e1e4f502d
kboul <kos.voulgaridis@gmail.com>
2022-12-09 23:45:55 +0200

    Apply less padding on LeftPanel container for small screens

--- src/components/UserPanel/LeftPanel.tsx
+++ src/components/UserPanel/LeftPanel.tsx
@@ -2,7 +2,7 @@ import UserCard from "../UserCard";
 
 export default function LeftPanel() {
   return (
-    <div className="bg-white h-[60%] sm:h-screen w-1/3 sm:w-4/12 p-3 overflow-y-auto">
+    <div className="bg-white h-[60%] sm:h-screen w-1/3 sm:w-4/12 p-3 sm:p-1 overflow-y-auto">
       <UserCard />
       <UserCard />
       <UserCard />
