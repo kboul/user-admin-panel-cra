33f16b4fa435ff253e3bb6f4f579d60f15330f55
kboul <kos.voulgaridis@gmail.com>
2022-12-10 19:54:46 +0200

    Prevent page refresh upon btns click

--- src/components/UserPanel/RightPanel/RightPanel.tsx
+++ src/components/UserPanel/RightPanel/RightPanel.tsx
@@ -8,6 +8,8 @@ import { useUserPanelContext } from "../userPanelContext";
 import { User } from "../../../models";
 import { labels } from "./constants";
 
+type ClickEvent = React.MouseEvent<HTMLElement>;
+
 export default function RightPanel() {
   const id = useId();
   const { selectedUserId } = useUserPanelContext();
@@ -16,7 +18,11 @@ export default function RightPanel() {
   let content;
   if (!selectedUserId) content = <CenteredText text="Select a user to edit" />;
   if (isFetching) content = <CenteredText text="Loading user..." />;
-  if (selectedUserId && user)
+  if (selectedUserId && user) {
+    const handleSaveClick = (e: ClickEvent) => e.preventDefault();
+
+    const handleCancelClick = (e: ClickEvent) => e.preventDefault();
+
     content = (
       <form className="flex flex-col m-8 mt-10 sm:m-5">
         {labels.map(({ label, key }) => (
@@ -29,15 +35,21 @@ export default function RightPanel() {
         ))}
 
         <div className="flex justify-end">
-          <Button className="bg-cancel-btn-bg hover:bg-cancel-btn-bg-hover text-black mr-2">
+          <Button
+            className="bg-cancel-btn-bg hover:bg-cancel-btn-bg-hover text-black mr-2"
+            onClick={handleCancelClick}>
             Cancel
           </Button>
-          <Button className="bg-save-btn-bg hover:bg-save-btn-bg-hover text-white">
+          <Button
+            className="bg-save-btn-bg hover:bg-save-btn-bg-hover text-white"
+            onClick={handleSaveClick}>
             Save
           </Button>
         </div>
       </form>
     );
+  }
+
   return (
     <div className="bg-white h-[60%] sm:h-screen w-1/3 sm:w-full">
       {content}
